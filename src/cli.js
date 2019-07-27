5a16e7b300eff84cf2b10d8c10fc84a3e95655b5
BosEriko <bos.eriko@gmail.com>
2019-07-27 17:10:58 +0800

    moved the parsing back

--- src/cli.js
+++ src/cli.js
@@ -2,7 +2,25 @@ import arg from 'arg';
 import inquirer from 'inquirer';
 
 function parseArgumentsIntoOptions(rawArgs) {
-
+    const args = arg(
+        {
+            '--git': Boolean,
+            '--yes': Boolean,
+            '--install': Boolean,
+            '-g': '--git',
+            '-y': '--yes',
+            '-i': '--install',
+        },
+        {
+            argv: rawArgs.slice(2),
+        }
+    );
+    return {
+        skipPrompts: args['--yes'] || false,
+        git: args['--git'] || false,
+        template: args._[0],
+        runInstall: args['--install'] || false,
+    };
 }
 
 async function promptForMissingOptions(options) {
