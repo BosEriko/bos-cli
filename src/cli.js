d6450744aed1f4b10276b6e7c9644fc4dbb2bb8f
BosEriko <bos.eriko@gmail.com>
2019-07-27 17:24:38 +0800

    fix indents

--- src/cli.js
+++ src/cli.js
@@ -26,37 +26,37 @@ function parseArgumentsIntoOptions(rawArgs) {
 async function promptForMissingOptions(options) {
     const defaultTemplate = 'JavaScript';
     if (options.skipPrompts) {
-    return {
-        ...options,
-        template: options.template || defaultTemplate,
-    };
+        return {
+            ...options,
+            template: options.template || defaultTemplate,
+        };
     }
 
     const questions = [];
     if (!options.template) {
-    questions.push({
-        type: 'list',
-        name: 'template',
-        message: 'Please choose which project template to use',
-        choices: ['JavaScript', 'TypeScript'],
-        default: defaultTemplate,
-    });
+        questions.push({
+            type: 'list',
+            name: 'template',
+            message: 'Please choose which project template to use',
+            choices: ['JavaScript', 'TypeScript'],
+            default: defaultTemplate,
+        });
     }
 
     if (!options.git) {
-    questions.push({
-        type: 'confirm',
-        name: 'git',
-        message: 'Initialize a git repository?',
-        default: false,
-    });
+        questions.push({
+            type: 'confirm',
+            name: 'git',
+            message: 'Initialize a git repository?',
+            default: false,
+        });
     }
 
     const answers = await inquirer.prompt(questions);
     return {
-    ...options,
-    template: options.template || answers.template,
-    git: options.git || answers.git,
+        ...options,
+        template: options.template || answers.template,
+        git: options.git || answers.git,
     };
 }
 
