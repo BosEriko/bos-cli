d5a3b2e008dc02c932d77187a1e5e1096c88e0e3
BosEriko <bos.eriko@gmail.com>
2019-08-04 17:50:35 +0800

    add if on main.js

--- src/main.js
+++ src/main.js
@@ -27,6 +27,15 @@ async function initGit(options) {
 }
 
 export async function bosOptions(options) {
+    if (options.state === 'Assist') {
+        console.log(1);
+    } else if (options.state === 'Directory') {
+        console.log(2);
+    } else if (options.state === 'Fun') {
+        console.log(3);
+    } else if (options.state === 'Help') {
+        console.log(4);
+    }
     // options = {
     //     ...options,
     //     targetDirectory: options.targetDirectory || process.cwd(),
